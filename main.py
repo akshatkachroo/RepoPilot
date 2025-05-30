0a27f3cf2eb29f309e0f11350b0fbf94450b9cbb
Akshat Kachroo <kachrooakshat@gmail.com>
2025-05-30 00:08:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-from dotenv import load_dotenv
-import os
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
-
-# Load environment variables
 
 
 
